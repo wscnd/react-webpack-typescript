54c2dfab38e31ea3368f47e6a4313ac64468d703
wscnd <wellingson@gmail.com>
2021-06-03 23:05:03 -0300

    Webpack: hotmodule

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./styles.css";
 import IMAGE from "./react.png";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
   return (
     <>
-      <h1>React Typescript Webpack Template </h1>
+      <h1>React Typescript Webpack Template</h1>
       <h2>
         {process.env.NODE_ENV}: {process.env.name}
       </h2>
       <img src={IMAGE} alt={"React Logo"} width="300" height="200" />
+      <ClickCounter />
     </>
   );
 };
