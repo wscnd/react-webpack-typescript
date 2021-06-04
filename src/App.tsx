598c24898342c30937c7264996708b857db72145
wscnd <wellingson@gmail.com>
2021-06-03 22:23:51 -0300

    Webpack: config for multiple environments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import IMAGE from "./react.png";
 export const App = () => {
   return (
     <>
-      <h1>React Typescript Webpack Template</h1>
+      <h1>React Typescript Webpack Template </h1>
+      <h2>
+        {process.env.NODE_ENV}: {process.env.name}
+      </h2>
       <img src={IMAGE} alt={"React Logo"} width="300" height="200" />
     </>
   );
